84c03ed40bb7c0d7602e22e66a426b9b66b3e823
Jonathon Cwik <jrblkbelt@comcast.net>
2015-01-24 20:50:21 -0600

    fixed datetimes that were 6 hours off

--- MetraAPI/Models/TrainSchedule.cs
+++ MetraAPI/Models/TrainSchedule.cs
@@ -12,8 +12,10 @@ namespace MetraAPI.Models
         public float MITPositionM { get; set; }
         public int TrainNumber { get; set; }
         public int Direction { get; set; }
-        public DateTime StopTime { get; set; }
-        public DateTime EstStopTime { get; set; }
+        private DateTime _stopTime { get; set; }
+        public DateTime StopTime { get { return _stopTime.AddHours(-6); } set { _stopTime = value; } }
+        private DateTime _estStopTime { get; set; }
+        public DateTime EstStopTime { get { return _estStopTime.AddHours(-6); } set { _estStopTime = value; } }
         public bool WasModified { get; set; }
         public string StopType { get; set; }
         public TrainStation Station { get; set; }
